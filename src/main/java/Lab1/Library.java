6657978112625327911e49cdde34b8001be653b0
JT <JT@10.89.139.70>
2022-09-19 22:19:58 +0800

    changes

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -7,7 +7,7 @@ public class Library {
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if( lib.someLibraryMethod() ) 
-			System.out.println("Hellow World!")
+			System.out.println("Hellow World!");
 		
 	}
 	
